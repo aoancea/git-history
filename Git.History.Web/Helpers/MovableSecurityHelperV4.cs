404c935730a22956d2fabd8aeca5b281c3fe8519
aoancea <alx.oancea@gmail.com>
2016-07-10 13:50:33 +0300

    Add Helper v4

---
+++ Git.History.Web/Helpers/MovableSecurityHelperV4.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Git.History.Web.Helpers
+{
+    public class MovableSecurityHelperV4
+    {
+    }
+}
