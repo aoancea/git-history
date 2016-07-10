ecf58b274bb2958c6ee30e04ad22b6867db0038e
aoancea <alx.oancea@gmail.com>
2016-07-10 13:20:12 +0300

    Rename and Move HelperTemp v3 to Helpers folder

--- Git.History.Web/Helpers/MovableSecurityHelperV3.cs
+++ Git.History.Web/Helpers/MovableSecurityHelperV3.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Git.History.Web.Helpers
+﻿namespace Git.History.Web.Helpers
 {
-    public class MovableSecurityHelperTempV3
+    public class MovableSecurityHelperV3
     {
         public void Method1()
         {
