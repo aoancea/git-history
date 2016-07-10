162970b1b3ba8b1d9daf183edc88be35639f16de
aoancea <alx.oancea@gmail.com>
2016-07-10 10:25:50 +0300

    Rename Filter to Helper v1

--- Git.History.Web/Helpers/MovableSecurityHelperV1.cs
+++ Git.History.Web/Helpers/MovableSecurityHelperV1.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Git.History.Web.Filters
+﻿namespace Git.History.Web.Helpers
 {
-    public class MovableSecurityFilterV1
+    public class MovableSecurityHelperV1
     {
         public void Method1()
         {
